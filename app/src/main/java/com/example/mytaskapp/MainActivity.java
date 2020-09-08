f72670ae1c09a97eee0465597dd4bc309fceb788
Roshan Nayak <roshannayak610@gmail.com>
2020-09-08 21:30:59 +0530

    task 7

--- app/src/main/java/com/example/mytaskapp/MainActivity.java
+++ app/src/main/java/com/example/mytaskapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.mytaskapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -16,6 +19,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.d("main", "in onCreate");
+
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction addFragmentOne = fragmentManager.beginTransaction();
+        Fragment fragment1 = new FirstFragment();
+        addFragmentOne.add(R.id.fragment, fragment1);
+        addFragmentOne.commit();
+
+
+        Log.d("test1", "in onCreate");
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction toggleFragments = fragmentManager.beginTransaction();
+
+                Fragment fragment = fragmentManager.findFragmentById(R.id.fragment);
+
+                if (fragment instanceof FirstFragment) {
+                    Fragment fragment1 = new SecondFragment();
+                    toggleFragments.replace(R.id.fragment, fragment1);
+                    toggleFragments.addToBackStack(null);
+                } else {
+                    Fragment fragment1 = new FirstFragment();
+                    toggleFragments.replace(R.id.fragment, fragment1);
+                    toggleFragments.addToBackStack(null);
+                }
+                toggleFragments.commit();
+            }
+        });
+
     }
 
     @Override
