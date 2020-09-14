d1ed4144ca7d8300fe0d89ffb5f81644147984cd
rishabh-jain-1990 <68217946+rishabh-jain-1990@users.noreply.github.com>
2020-09-14 20:15:20 +0530

    Task 5 (#11)
    * task 5
    
    * task 6 (#3)
    
    * task 6
    
    * task 6_2
    
    Co-authored-by: Roshan Nayak <roshannayak610@gmail.com>
    
    Co-authored-by: Roshan Nayak <roshannayak610@gmail.com>
    Co-authored-by: NayakRoshan <70518707+NayakRoshan@users.noreply.github.com>

--- app/src/main/java/com/example/mytaskapp/MainActivity.java
+++ app/src/main/java/com/example/mytaskapp/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.mytaskapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
