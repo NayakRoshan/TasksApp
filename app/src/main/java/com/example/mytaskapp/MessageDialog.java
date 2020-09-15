a13b631dfdb3312d9d77fd6c365567ba82dcdbc6
Roshan Nayak <roshannayak610@gmail.com>
2020-09-15 12:17:02 +0530

    *redone task 8

--- app/src/main/java/com/example/mytaskapp/MessageDialog.java
+++ app/src/main/java/com/example/mytaskapp/MessageDialog.java
@@ -14,7 +14,6 @@ public class MessageDialog extends DialogFragment {
 
     private Context context;
     private String message;
-    private String DIALOG_TITLE = "Alert Message";
 
     public MessageDialog(Context context, String message) {
         this.context = context;
@@ -26,8 +25,8 @@ public class MessageDialog extends DialogFragment {
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         AlertDialog.Builder messageDialog = new AlertDialog.Builder(context);
         messageDialog.setMessage(message)
-                .setTitle(DIALOG_TITLE)
-                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                .setTitle(getString(R.string.dialog_box_title))
+                .setPositiveButton(getString(R.string.positive_button), new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.dismiss();
